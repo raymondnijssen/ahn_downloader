bd42c41aa20d5d3655be3a8a6566e05cb033f822
Raymond Nijssen <r.nijssen@terglobo.nl>
2020-09-05 18:16:55 +0200

    use cache for finding file sizes

--- ahn_downloader.py
+++ ahn_downloader.py
@@ -1,6 +1,7 @@
 import urllib.request, urllib.error
 import os.path
 import ssl
+import csv
 
 '''
 example url's
@@ -14,6 +15,9 @@ https://download.pdok.nl/rws/ahn3/v1_0/laz/C_45CN2.LAZ
 _BASE_URL = 'https://download.pdok.nl/rws/ahn3/v1_0/{}/{}'
 _TYPE_CODES = {'laz': 'C', 'dtm': 'M', 'dsm': 'R'}
 
+_FILE_SIZE_CSV = 'bladindex_metadata.csv'
+_FILE_SIZES = {}
+
 class AhnDownloader:
 
     def __init__(self, map_sheet, resolution='5', dem_type='dtm', ssl_check=True):
@@ -21,6 +25,17 @@ class AhnDownloader:
         self.resolution = resolution # '5' or '05'
         self.dem_type = dem_type.lower()
         self.ssl_check = ssl_check
+        if len(_FILE_SIZES) == 0:
+            self.load_file_sizes()
+
+    def load_file_sizes(self):
+        print('loading file sizes')
+        with open(_FILE_SIZE_CSV) as file_size_csv:
+            csv_reader = csv.DictReader(file_size_csv)
+            for in_rec in csv_reader:
+                bladnr = in_rec['bladnr'].upper()
+                _FILE_SIZES[bladnr] = in_rec
+        #print(_FILE_SIZES)
 
 
     def create_url(self):
@@ -74,7 +89,15 @@ class AhnDownloader:
                 return None
 
 
-    def get_remote_file_size(self):
+    def get_remote_file_size(self, use_cache=True):
+        if use_cache:
+            if self.dem_type == 'laz':
+                field_name = 'laz_size'
+            else:
+                field_name = f'{self.dem_type}_{self.resolution}_size'
+            if self.map_sheet in _FILE_SIZES:
+                size = int(_FILE_SIZES[self.map_sheet][field_name])
+                return size
         url, fn = self.create_url()
         response = self.get_response(url)
         if response is None:
@@ -108,4 +131,6 @@ class AhnDownloader:
 if __name__ == '__main__':
     ad = AhnDownloader('45cn1', resolution='5')
     print(ad.create_url())
+    ad.download('/home/raymond/tmp/ahn3')
+    ad = AhnDownloader('01CZ2', resolution='5')
     ad.download('/home/raymond/tmp/ahn3')
